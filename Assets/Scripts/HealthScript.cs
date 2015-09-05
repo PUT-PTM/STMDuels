d3b9f058c393634ded9af28372a07c7d9e1cb373
Filip Kaszczyński <graferubp@gmail.com>
2015-09-05 06:20:30 +0200

    Shots & blood
    - Added sound of firing a gun
    - Added blood splatter prefab with the script
    - Added blood splatter sprite on character when hit
    - - Duration of showing a sprite is determined by a public variable
    - Updated all of the characters to the current scripts and character's
    assets

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class healthScript : MonoBehaviour {
 
 	public int hp = 10;
+	public GameObject bloodSplatter;
 
 	void OnTriggerEnter2D(){
 			
+		Instantiate (bloodSplatter, transform.position, transform.rotation);
+
 		hp -= 1;
 
 		if (hp <= 0)
