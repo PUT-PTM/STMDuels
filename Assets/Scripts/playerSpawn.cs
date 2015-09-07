cdd0da2f054f387680453ea5fc812cdeaf16bb3b
Filip Kaszczyński <graferubp@gmail.com>
2015-09-08 00:51:20 +0200

    Spawns + fix
    Instead of spawning characters, there are now spawn spots for 4 players.
    They'll receive variables with information about number of players and
    characters chosen by them.
    
    + fixed wall / hedges colliders

---
+++ Assets/Scripts/playerSpawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerSpawn : MonoBehaviour {
+
+	public GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (character, transform.position, transform.rotation);	
+	}
+
+}
