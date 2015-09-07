6d670357debb78780acd2a7948143429e68f6044
Filip Kaszczyński <graferubp@gmail.com>
2015-09-07 23:41:27 +0200

    Bullets Trails
    Added trails following bullets
    - Now you can see which direction bullet is going
    - Trails work with proper and dynamic rotation

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -7,11 +7,9 @@ public class bulletMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
 		Vector3 pos = transform.position;
 		Vector3 velocity = new Vector3 (0, maxSpeed * Time.deltaTime, 0);
 		pos += transform.rotation * velocity;
-		transform.position = pos;
-	
+		transform.position = pos;	
 	}
 }
