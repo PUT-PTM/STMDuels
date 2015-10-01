310a31663e8864fdc551fb21926b7f1efa4c9877
wilq94 <paba.lech@poczta.fm>
2015-10-01 17:32:14 +0200

    Last update
    Final version of the code with the recent updates.

---
+++ STMDuels/Assets/menuVolume.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuVolume : MonoBehaviour {
+
+    public GameObject musicBackground;
+
+    void Start()
+    {
+
+        
+       
+       musicBackground.gameObject.GetComponent<AudioSource>().Play();
+        
+    }
+
+
+    void Update ()
+    {
+
+        musicBackground.gameObject.GetComponent<AudioSource>().volume = 0.0f;
+
+    }
+    // Use this for initialization
+
+}
